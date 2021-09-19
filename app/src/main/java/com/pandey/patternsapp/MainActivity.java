39ec79f638fa44423a48e6740baa6032e7bcbcbd
h4xor7 <saurabhpandey436@gmail.com>
2021-09-19 13:40:55 +0530

    implemented mvc  architecture Model view Controller

--- app/src/main/java/com/pandey/patternsapp/MainActivity.java
+++ app/src/main/java/com/pandey/patternsapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.pandey.patternsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+/**
+ * Model is MyModel
+ * View is our xml
+ * Activity is our controller*/
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        textView= findViewById(R.id.textView);
+
+
+    }
+
+    public  MyModel getAppFromModel(){
+
+        return new MyModel("Pattern App",400,5);
+    }
+
+    public void displayText(View view) {
+        textView.setText(getAppFromModel().appName+"Downloads"+getAppFromModel().appDownloads+"Ratings"+getAppFromModel().appRating );
     }
 }
---
+++ app/src/main/java/com/pandey/patternsapp/MyModel.java
@@ -0,0 +1,42 @@
+package com.pandey.patternsapp;
+
+public class MyModel {
+
+    String appName;
+    int appDownloads;
+    int appRating;
+
+    public MyModel(String appName, int appDownloads, int appRating) {
+        this.appName = appName;
+        this.appDownloads = appDownloads;
+        this.appRating = appRating;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public int getAppDownloads() {
+        return appDownloads;
+    }
+
+    public void setAppDownloads(int appDownloads) {
+        this.appDownloads = appDownloads;
+    }
+
+    public int getAppRating() {
+        return appRating;
+    }
+
+    public void setAppRating(int appRating) {
+        this.appRating = appRating;
+    }
+
+
+
+
+}
